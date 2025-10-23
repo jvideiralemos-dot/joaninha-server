2a3d76e8b4c80fb00dd9007eb8683e61562941b8
jvideiralemos-dot <jvideiralemos@gmail.com>
2025-10-22 22:12:47 -0300

    Update whatsapp.js

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -1,1 +1,12 @@
+// routes/whatsapp.js
+const express = require("express");
+const router = express.Router();
+
+// rota de teste (depois substituímos pelo fluxo real)
+router.post("/", (req, res) => {
+  console.log("Mensagem recebida:", req.body);
+  res.status(200).send("Mensagem processada com sucesso!");
+});
+
+module.exports = router;
 
